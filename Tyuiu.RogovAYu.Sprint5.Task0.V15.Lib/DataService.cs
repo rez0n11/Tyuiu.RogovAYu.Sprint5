f9c7884aecee42911bc3c49c4ae81784eaea43eb
REZ0N11\rezon <rez0n11@ya.ru>
2024-11-25 04:17:41 +0500

    Add project files.

---
+++ Tyuiu.RogovAYu.Sprint5.Task0.V15.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.RogovAYu.Sprint5.Task0.V15.Lib
+{
+    public class DataService : ISprint5Task0V15
+    {
+        public string SaveToFileTextData(int x)
+        {
+           string file = Path.GetTempFileName();
+            string res="";
+            res = Convert.ToString(Math.Round(x / (x * x * x + 2.0), 3));
+           File.WriteAllText(file, res.ToString());
+            return file;
+        }
+    }
+}
---
+++ Tyuiu.RogovAYu.Sprint5.Task0.V15/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using Tyuiu.RogovAYu.Sprint5.Task0.V15.Lib;
+
+namespace Tyuiu.RogovAYu.Sprint.Task.V
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DataService ds = new DataService();
+            string result;
+
+            Console.Title = "Task:5.0.v15| Рогов А.Ю., ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #15                                                              *");
+            Console.WriteLine("* Выполнил: Рогов Александр Юрьевич |  ПКТб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:          *\n");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.Write("* x = 3                                                                ");
+            
+            Console.WriteLine("***************************************************************************");
+
+            { //code
+                result = ds.SaveToFileTextData(3);
+            }
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"* Файл создан: "+result);
+            Console.WriteLine("***************************************************************************");
+            Console.ReadKey();
+        }
+    }
+}
