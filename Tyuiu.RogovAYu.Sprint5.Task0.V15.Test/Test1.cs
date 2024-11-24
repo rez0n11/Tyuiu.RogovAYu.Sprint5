f0cc14ce1cae08213ce5aedace6e7706961409f7
REZ0N11\rezon <rez0n11@ya.ru>
2024-11-25 04:32:12 +0500

    0

---
+++ Tyuiu.RogovAYu.Sprint5.Task0.V15.Test/Test1.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.RogovAYu.Sprint5.Task0.V15.Lib;
+namespace Tyuiu.RogovAYu.Sprint5.Task0.V15.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string a = ds.SaveToFileTextData(3);
+            FileInfo fileInfo = new FileInfo(a);
+            Assert.AreEqual(true, fileInfo.Exists);
+        }
+    }
+}
