c7c68aea0a2ae1c42a0f226aebf9d3993b5365d2
REZ0N11\rezon <rez0n11@ya.ru>
2024-11-27 23:56:40 +0500

    6

---
+++ Tyuiu.RogovAYu.Sprint5.Task6.V24.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.RogovAYu.Sprint5.Task6.V24.Lib
+{
+    public class DataService : ISprint5Task6V24
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int i = 0, k = 0;
+            string[] a = File.ReadAllText(path).Split(' ');
+            foreach (string s in a)
+            {
+                if (int.TryParse(s,out k)&& s.Length==5)
+                {
+                   i++;
+                }           
+            }
+            return i;
+        }
+    }
+}
---
+++ Tyuiu.RogovAYu.Sprint5.Task6.V24.Test/Test1.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.RogovAYu.Sprint5.Task6.V24.Lib;
+namespace Tyuiu.RogovAYu.Sprint5.Task6.V24.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new();
+            Assert.AreEqual(2,ds.LoadFromDataFile(Path.Combine("C:","DataSprint5","InPutDataFileTask6V24.txt")));
+        }
+    }
+}
