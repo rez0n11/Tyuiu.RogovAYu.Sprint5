04ab552537f72d2eed4dc38a9bdb89af5c5641c1
REZ0N11\rezon <rez0n11@ya.ru>
2024-11-27 21:02:24 +0500

    5

--- Tyuiu.RogovAYu.Sprint5.Task5.V27.Lib/DataService.cs
+++ Tyuiu.RogovAYu.Sprint5.Task5.V27.Lib/DataService.cs
@@ -6,20 +6,18 @@ namespace Tyuiu.RogovAYu.Sprint5.Task5.V27.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            using (StreamReader sr = new StreamReader(path))
+            double r = 0; int i = 0;
+            using StreamReader sr = new StreamReader(path);
+            string line;
+            while ((line = sr.ReadLine()) != null)
             {
-                double res=0;
-                int i= 0;
-                string line;
-                while ((line = sr.ReadLine()) != null && Convert.ToDouble(line)%5==0)
+                if (Convert.ToDouble(line)%5==0)
                 {
-                    res += Convert.ToDouble(line);
+                    r += Convert.ToDouble(line);
                     i++;
                 }
-                return (res / i);
             }
-
-            
+            return r/i;
         }
     }
 }
