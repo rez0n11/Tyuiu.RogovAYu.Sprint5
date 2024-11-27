4a28202961c0aebf6bb15c5c996cb0b1dbeb4cfd
REZ0N11\rezon <rez0n11@ya.ru>
2024-11-27 05:00:22 +0500

    4

--- Tyuiu.RogovAYu.Sprint5.Task4.V3.Lib/DataService.cs
+++ Tyuiu.RogovAYu.Sprint5.Task4.V3.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.RogovAYu.Sprint5.Task4.V3.Lib
         public double LoadFromDataFile(string path)
         {
 
-            double x = Convert.ToDouble(File.ReadAllText(path));
+            double x = Convert.ToDouble(File.ReadAllText(path).Replace('.',','));
             x = Math.Round((Math.Sin(x)+4)/x-x*1.25,3);
             return x;
         }
